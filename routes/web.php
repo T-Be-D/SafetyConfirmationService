99c3aa0552e0ac0c511cad41e02bca3297e3f1a4
Eakudompong Chanoknan <ceguantz96@outlook.com>
2023-04-28 12:06:48 +0900

    studentIDとpasswordに基づいてユーザーベースを認証 Routing 修理

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('auth.login');
+    return redirect('login');
 });
 
 Route::get('/dashboard', function () {
