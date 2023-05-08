07054c7c1a7d8384620050aacbf69a1b588fe681
Eakudompong Chanoknan <ceguantz96@outlook.com>
2023-05-08 15:27:04 +0900

    change title

--- resources/views/listview.blade.php
+++ resources/views/listview.blade.php
@@ -31,7 +31,7 @@ $classes = array_unique($classes);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Listview</title>
+    <title>災害掲示板</title>
     @vite('resources/css/app.css')
 </head>
 
