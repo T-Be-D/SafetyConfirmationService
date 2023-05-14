a94908dba040de714a854d402c2343478f079726
miti <2220399@ecc.ac.jp>
2023-05-15 02:50:20 +0900

    change

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -11,15 +11,22 @@
                 <x-input-error :messages="$errors->get('student_id')" class="mt-2" />
             </div>
 
-            <!-- Class 項目追加のみ-->
-            {{-- <div>
-                <x-input-label for="class" :value="__('Class')" />
-                <x-text-input id="class" class="block mt-1 w-50" type="text" name="class" :value="old('class')"
-                    required autofocus autocomplete="class" />
-                <x-input-error :messages="$errors->get('class')" class="mt-2" />
-            </div> --}}
             <div>
                 <x-input-label for="class" :value="__('Class')" />
+                <div class="flex gap-4">
+                    <div class="flex">
+                        <input type="radio" id="it_radio" name="category" value="IT" checked>
+                        <label for="it_radio" style="font-size: 18px">IT</label>
+                    </div>
+                    <div class="flex">
+                        <input type="radio" id="game_radio" name="category" value="GAME">
+                        <label for="game_radio" style="font-size: 18px">ゲーム</label>
+                    </div>
+                    <div class="flex">
+                        <input type="radio" id="other_radio" name="category" value="other">
+                        <label for="other_radio" style="font-size: 18px">その他</label>
+                    </div>
+                </div>
                 <div class="flex gap-2">
                     <select id="class_type" name="class_type"
                         class="block mt-1 w-50 rounded dark:bg-gray-700 dark:text-white " required autofocus
@@ -32,90 +39,339 @@
                         class="block mt-1 w-50 rounded dark:bg-gray-700 dark:text-white" required autofocus
                         autocomplete="class">
                     </select>
-
                 </div>
-
                 <x-input-error :messages="$errors->get('class')" class="mt-2" />
             </div>
-            <script>
-                const classTypeSelect = document.getElementById('class_type');
-                const classSelect = document.getElementById('class');
+        </div>
+        <script>
+            const classTypeSelect = document.getElementById('class_type');
+            const classSelect = document.getElementById('class');
 
-                const classOptions = {
-                    IE: [{
-                            value: 'IE1A',
-                            label: 'IE1A'
-                        },
-                        {
-                            value: 'IE1B',
-                            label: 'IE1B'
-                        },
-                        {
-                            value: 'IE2A',
-                            label: 'IE2A'
-                        },
-                        {
-                            value: 'IE2B',
-                            label: 'IE2B'
-                        },
-                        {
-                            value: 'IE3A',
-                            label: 'IE3A'
-                        },
-                        {
-                            value: 'IE3B',
-                            label: 'IE3B'
-                        }
-                    ],
-                    SK: [{
-                            value: 'SK1A',
-                            label: 'SK1A'
-                        },
-                        {
-                            value: 'SK1B',
-                            label: 'SK1B'
-                        },
-                        {
-                            value: 'SK2A',
-                            label: 'SK2A'
-                        },
-                        {
-                            value: 'SK2B',
-                            label: 'SK2B'
-                        },
-                        {
-                            value: 'SK3A',
-                            label: 'SK3A'
-                        },
-                        {
-                            value: 'SK3B',
-                            label: 'SK3B'
-                        }
-                    ],
-                    teacher: [{
-                            value: '校長',
-                            label: '校長'
-                        },
-                        {
-                            value: '教頭',
-                            label: '教頭'
-                        },
-                        {
-                            value: '教員',
-                            label: '教員'
-                        }
-                    ]
-                };
+            const classOptions = {
+                IT: [{
+                        value: 'IE',
+                        label: 'IE'
+                    },
+                    {
+                        value: 'SK',
+                        label: 'SK'
+                    },
+                ],
+                GAME: [{
+                    value: 'GE',
+                    label: 'GE'
+                }, ],
 
-                // Update the options in the class select element
-                function updateClassOptions() {
-                    const selectedClassType = classTypeSelect.value;
-                    const options = classOptions[selectedClassType];
+                other: [{
+                    value: 'teacher',
+                    label: '教員'
+                }, {
+                    value: 'high',
+                    label: '高校'
+                }, ],
+            };
 
+            // Update the options in the class_type select element
+            function updateClassTypeOptions() {
+                const selectedCategory = document.querySelector('input[name="category"]:checked').value;
+                const options = classOptions[selectedCategory];
+
+                // Clear current options
+                classTypeSelect.innerHTML = '';
+
+                // Add new options
+                options.forEach(option => {
+                    const {
+                        value,
+                        label
+                    } = option;
+                    const optionElement = document.createElement('option');
+                    optionElement.value = value;
+                    optionElement.textContent = label;
+                    classTypeSelect.appendChild(optionElement);
+                });
+
+                // Trigger the change event to update the class select options
+                classTypeSelect.dispatchEvent(new Event('change'));
+            }
+
+            // Add event listener for category radio buttons
+            const categoryRadios = document.querySelectorAll('input[name="category"]');
+            categoryRadios.forEach(radio => {
+                radio.addEventListener('change', updateClassTypeOptions);
+            });
+
+            // Initialize the class_type options
+            updateClassTypeOptions();
+
+            // Update the options in the class select element
+            function updateClassOptions() {
+                const selectedClassType = classTypeSelect.value;
+                const options = classOptions[selectedClassType];
+
+                // Clear current options
+                classSelect.innerHTML = '';
+
+                // Add new options
+                options.forEach(option => {
+                    const {
+                        value,
+                        label
+                    } = option;
+                    const optionElement = document.createElement('option');
+                    optionElement.value = value;
+                    optionElement.textContent = label;
+                    classSelect.appendChild(optionElement);
+                });
+            }
+
+            // Add event listener for class type select
+            classTypeSelect.addEventListener('change', updateClassOptions);
+
+            const classOptionsIE = [{
+                    value: 'IE1A',
+                    label: 'IE1A'
+                },
+                {
+                    value: 'IE1B',
+                    label: 'IE1B'
+                },
+                {
+                    value: 'IE2A',
+                    label: 'IE2A'
+                },
+                {
+                    value: 'IE2B',
+                    label: 'IE2B'
+                },
+                {
+                    value: 'IE3A',
+                    label: 'IE3A'
+                },
+                {
+                    value: 'IE3B',
+                    label: 'IE3B'
+                },
+                {
+                    value: 'IE4A',
+                    label: 'IE4A'
+                },
+                {
+                    value: 'IE4B',
+                    label: 'IE4B'
+                },
+            ];
+
+            const classOptionsSK = [{
+                    value: 'SK1A',
+                    label: 'SK1A'
+                },
+                {
+                    value: 'SK1B',
+                    label: 'SK1B'
+                },
+                {
+                    value: 'SK2A',
+                    label: 'SK2A'
+                },
+                {
+                    value: 'SK2B',
+                    label: 'SK2B'
+                },
+                {
+                    value: 'SK3A',
+                    label: 'SK3A'
+                },
+                {
+                    value: 'SK3B',
+                    label: 'SK3B'
+                },
+            ];
+
+            const classOptionsGE = [{
+                    value: 'GE1A',
+                    label: 'GE1A'
+                },
+                {
+                    value: 'GE1B',
+                    label: 'GE1B'
+                },
+                {
+                    value: 'GE1C',
+                    label: 'GE1C'
+                },
+                {
+                    value: 'GE2A',
+                    label: 'GE2A'
+                },
+                {
+                    value: 'GE2B',
+                    label: 'GE2B'
+                },
+                {
+                    value: 'GE2C',
+                    label: 'GE2C'
+                },
+                {
+                    value: 'GE3A',
+                    label: 'GE3A'
+                },
+                {
+                    value: 'GE3B',
+                    label: 'GE3B'
+                },
+                {
+                    value: 'GE3C',
+                    label: 'GE3C'
+                },
+                {
+                    value: 'GE4A',
+                    label: 'GE4A'
+                },
+                {
+                    value: 'GE4B',
+                    label: 'GE4B'
+                },
+                {
+                    value: 'GE4C',
+                    label: 'GE4C'
+                },
+
+            ];
+
+
+
+            const classOptionsOther = [{
+                    value: '校長',
+                    label: '校長'
+                },
+                {
+                    value: '教頭',
+                    label: '教頭'
+                },
+                {
+                    value: '講師',
+                    label: '講師'
+                },
+            ];
+
+            const classOptionshigh = [{
+                    value: 'HG1A',
+                    label: 'HG1A'
+                },
+                {
+                    value: 'HG2A',
+                    label: 'HG2A'
+                },
+                {
+                    value: 'HG3A',
+                    label: 'HG3A'
+                },
+                {
+                    value: 'HC1A',
+                    label: 'HC1A'
+                },
+                {
+                    value: 'HC2A',
+                    label: 'HC2A'
+                },
+                {
+                    value: 'HC3A',
+                    label: 'HC3A'
+                },
+
+            ];
+
+
+
+            function updateClassOptions() {
+                const selectedClassType = classTypeSelect.value;
+
+                if (selectedClassType === 'IE') {
                     // Clear current options
                     classSelect.innerHTML = '';
 
-                    // Add new options
+                    // Add new options for IE
+                    classOptionsIE.forEach(option => {
+                        const {
+                            value,
+                            label
+                        } = option;
+                        const optionElement = document.createElement('option');
+                        optionElement.value = value;
+                        optionElement.textContent = label;
+                        classSelect.appendChild(optionElement);
+                    });
+                } else if (selectedClassType === 'SK') {
+                    // Clear current options
+                    classSelect.innerHTML = '';
+
+                    // Add new options for IE
+                    classOptionsSK.forEach(option => {
+                        const {
+                            value,
+                            label
+                        } = option;
+                        const optionElement = document.createElement('option');
+                        optionElement.value = value;
+                        optionElement.textContent = label;
+                        classSelect.appendChild(optionElement);
+                    });
+                } else if (selectedClassType === 'GE') {
+                    // Clear current options
+                    classSelect.innerHTML = '';
+
+                    // Add new options for IE
+                    classOptionsGE.forEach(option => {
+                        const {
+                            value,
+                            label
+                        } = option;
+                        const optionElement = document.createElement('option');
+                        optionElement.value = value;
+                        optionElement.textContent = label;
+                        classSelect.appendChild(optionElement);
+                    });
+
+                } else if (selectedClassType === 'teacher') {
+                    // Clear current options
+                    classSelect.innerHTML = '';
+
+                    // Add new options for IE
+                    classOptionsOther.forEach(option => {
+                        const {
+                            value,
+                            label
+                        } = option;
+                        const optionElement = document.createElement('option');
+                        optionElement.value = value;
+                        optionElement.textContent = label;
+                        classSelect.appendChild(optionElement);
+                    });
+
+                } else if (selectedClassType === 'high') {
+                    // Clear current options
+                    classSelect.innerHTML = '';
+
+                    // Add new options for IE
+                    classOptionshigh.forEach(option => {
+                        const {
+                            value,
+                            label
+                        } = option;
+                        const optionElement = document.createElement('option');
+                        optionElement.value = value;
+                        optionElement.textContent = label;
+                        classSelect.appendChild(optionElement);
+                    });
+
+
+                } else {
+                    // Clear current options
+                    classSelect.innerHTML = '';
+
+                    // Add new options based on selected class type
+                    const options = classOptions[selectedClassType];
                     options.forEach(option => {
                         const {
                             value,
@@ -127,20 +383,14 @@
                         classSelect.appendChild(optionElement);
                     });
                 }
+            }
 
-                // Add event listener for class type select
-                classTypeSelect.addEventListener('change', updateClassOptions);
+            // Add event listener for class type select
+            classTypeSelect.addEventListener('change', updateClassOptions);
 
-                // Initialize the class options
-                updateClassOptions();
-            </script>
-
-
-
-
-        </div>
-
-
+            // Initialize the class options
+            updateClassOptions();
+        </script>
         <!-- Name -->
         <div>
             <x-input-label for="name" :value="__('Name')" />
